fc8c099a22441f295a1e9d0a5215e43c172b7808
abhishek <abhi19982014@gmail.com>
2024-04-09 13:23:40 +0530

    9 aprl 13:23

--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -27,10 +27,11 @@ const Singup = () => {
   };
 
   const handleSubmit = async (e) => {
+    
     e.preventDefault();
 
     axios
-    //   .post(`${server}/user/create-user`, { name, email, password, avatar })
+      .post(`${server}/user/create-user`, { name, email, password, avatar })
       .then((res) => {
         toast.success(res.data.message);
         setName("");
