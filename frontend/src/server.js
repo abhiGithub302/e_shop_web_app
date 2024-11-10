052e9dfac4dbae7473771a01f70cc2d2083679f8
abhishek <abhi19982014@gmail.com>
2024-11-10 16:58:23 +0530

    backend url add

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
 export const server = "e-shop-web-app-backend.vercel.app/api/v2";
 
-export const backend_url = "e-shop-web-app-backend.vercel.app";
+export const backend_url = "e-shop-web-app-backend.vercel.app/";
 
